c72326f109d0706d51fe4ecfd7f17142eac61d94
yuta0601 <y.nakazawa3003+github@gmail.com>
2020-09-12 15:33:40 +0900

    [ADD] timer

---
+++ src/timer/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"fyne.io/fyne"
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/widget"
+	"github.com/nozo-moto/pomodoro_timer/timer"
+	"time"
+)
+
+var (
+	showTimeLabel widget.Label
+)
+
+const (
+	countTime = time.Second * 10
+)
+
+func init() {
+	showTimeLabel.Alignment = fyne.TextAlignCenter
+}
+func main() {
+	showTimeChan := make(chan time.Duration)
+	timer := timer.New(countTime, showTimeChan)
+	go timer.Run()
+	go func() {
+		for {
+			select {
+			case showtime := <-showTimeChan:
+				showTimeLabel.SetText(formatTime(showtime))
+			}
+		}
+	}()
+	startButton := &widget.Button{
+		Text: "Start", OnTapped: func() { timer.Start() },
+	}
+	stopButton := &widget.Button{
+		Text: "Stop", OnTapped: func() { timer.Stop() },
+	}
+	a := app.New()
+	w := a.NewWindow("Timer")
+	w.Resize(fyne.Size{Width: 150, Height: 150})
+	canvasObjects := []fyne.CanvasObject{
+		&widget.Label{Text: "Timer", Alignment: fyne.TextAlignCenter},
+		&showTimeLabel,
+		startButton,
+		stopButton,
+	}
+	w.SetContent(&widget.Box{Children: canvasObjects})
+	timer.Initialize(countTime)
+	w.ShowAndRun()
+}
+func formatTime(t time.Duration) string {
+	h := t / time.Hour
+	m := (t - h*time.Hour) / time.Minute
+	s := (t - h*time.Hour - m*time.Minute) / time.Second
+	return fmt.Sprintf("%d:%d:%d", h, m, s)
+}
